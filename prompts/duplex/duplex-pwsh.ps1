1578edd437abb2cf1878b083a739bcafa30ae1cc
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2019-03-24 23:14:16 -0400

    replace cwd with cwf

--- prompts/duplex/duplex-pwsh.ps1
+++ prompts/duplex/duplex-pwsh.ps1
@@ -3,83 +3,6 @@
 # ----- HELPER FUNCTIONS -------
 # stolen from Oh-My-Posh
 
-function Test-PsCore {
-    return $PSVersionTable.PSVersion.Major -gt 5
-}
-
-function Get-Drive {
-    param(
-        [Parameter(Mandatory = $true)]
-        [System.Object]
-        $dir
-    )
-
-    $provider = (Get-Item $dir.path -Force).PSProvider.Name
-
-    if($provider -eq 'FileSystem') {
-        $homedir = ($HOME.TrimEnd('/','\'))
-        if($dir.Path.StartsWith($homedir)) {
-            return '~'
-        }
-        elseif($dir.Path.StartsWith('Microsoft.PowerShell.Core')) {
-            $parts = $dir.Path.Replace('Microsoft.PowerShell.Core\FileSystem::\\','').Split('\')
-            return "$($parts[0])$($sl.PromptSymbols.PathSeparator)$($parts[1])$($sl.PromptSymbols.PathSeparator)"
-        }
-        else {
-            $root = $dir.Drive.Name
-            if($root) {
-                return $root
-            }
-            else {
-                return $dir.Path.Split(':\')[0] + ':'
-            }
-        }
-    }
-    else {
-        return $dir.Drive.Name
-    }
-}
-
-function Get-ShortPath {
-    param(
-        [Parameter(Mandatory = $true)]
-        [System.Management.Automation.PathInfo]
-        $dir
-    )
-
-    $provider = ((Get-Item $dir.path -Force).PSProvider.Name)
-
-    if($provider -eq 'FileSystem') {
-        $result = @()
-        $currentDir = Get-Item $dir.path -Force
-
-        while( ($currentDir.Parent) -And ($currentDir.FullName -ne (($HOME.TrimEnd('/','\')))) ) {
-            if( ( (Test-Path -Path "$($currentDir.FullName)\.git") -Or (Test-Path -Path "$($dir.FullName)\.hg") -Or (Test-Path -Path "$($dir.FullName)\.svn")) -Or ($result.length -eq 0) ) {
-                $result = ,$currentDir.Name + $result
-            }
-            else {
-                $result = ,'..' + $result
-            }
-
-            $currentDir = $currentDir.Parent
-        }
-        $shortPath =  $result -join '\'
-        if ($shortPath) {
-            $drive = (Get-Drive -dir $dir)
-            return "${drive}\$shortPath"
-        }
-        else {
-            if ($dir.path -eq (($HOME.TrimEnd('/','\')))) {
-                return '~'
-            }
-            return "$($dir.Drive.Name):"
-        }
-    }
-    else {
-        return $dir.path.Replace((Get-Drive -dir $dir), '')
-    }
-}
-
 function Get-ComputerName {
     if (Test-PsCore -and $PSVersionTable.Platform -ne 'Windows') {
         if ($env:NAME) {
@@ -95,7 +18,7 @@ function prompt {
     # variables
     $hostname = get-computername
     $username = [System.Environment]::UserName
-    $cwdr = get-shortpath $(pwd)
+    $cwdr = split-path $PWD -leaf
     
     # chars
     $dash = [text.encoding]::UTF8.getstring((226,148,128))
@@ -123,7 +46,7 @@ function prompt {
     $prompt2 = [text.encoding]::UTF8.getstring((226,148,148,226,148,128,226,148,128,226,149,188))
     write-host "${E}[0;39m${username}${E}[01;33m@${E}[01;96m${hostname}" -nonewline
     write-host "${E}[0;31m]${dash}[${E}[0;32m${cwdr}${E}[0;31m]" -nonewline
-    write-host "`n${E}[0;31m${prompt2} ${E}[0m${E}[01;33m`$${E}[0m"
+    write-host "`n${E}[0;31m${prompt2} ${E}[0m${E}[01;33m`$${E}[0m" -nonewline
     
     return " "
 }
